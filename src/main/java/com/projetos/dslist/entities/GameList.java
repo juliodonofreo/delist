11950702efffd4f3f725ba9037425d7babad50c0
Julio Donofreo <julio.bibico@hotmail.com>
2023-06-26 11:47:47 -0300

    Database seed update

--- src/main/java/com/projetos/dslist/entities/GameList.java
+++ src/main/java/com/projetos/dslist/entities/GameList.java
@@ -10,7 +10,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_list")
+@Table(name = "tb_game_list")
 public class GameList implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
